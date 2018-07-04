690c6b31bca9f2cbbbbbfefbbdc3b76cdaed4480
Marcel Rupp <marcel.rupp@live.de>
2018-07-05 00:35:26 +0200

    Finished up steering control and feedback LED

--- SecondaryController/SecondaryController.cpp
+++ SecondaryController/SecondaryController.cpp
@@ -6,7 +6,6 @@
 #define EXTEND 9
 #define RETRACT 11
 #define LED_DATA 7
-#define NUM_READINGS 10
 #define EEPROM_MIN_MAX_ADDR_OFFSET 0
 #define NUM_CHANNELS 2
 #define CHANNEL_1 0
@@ -18,15 +17,14 @@
 CRGB leds[1];
 
 int targetPos = 600;
-int deadBand = 5;
+int deadBand = 2;
 int total = 0;
-int curpos  = 0;
+int curPos = 0;
+int calibrateCount = 0;
 
 int min = -1;
 int max = -1;
 
-int readings[NUM_READINGS];
-int readIndex = 0;
 int lastPos = 0;
 int blockingCount = 0;
 bool blocking = false;
@@ -38,175 +36,171 @@ volatile int raw_inputs[NUM_CHANNELS];
 unsigned long current_time_int0, upflank_time[NUM_CHANNELS];
 unsigned long loop_timer;
 
-void limit_receiver_input(byte n) {
-  int rc_signal = raw_inputs[n];
-  if (rc_signal > 2000) rc_signal = 2000;
-  if (rc_signal < 1000) rc_signal = 1000;
-  receiver_input[n] = rc_signal;
-}
-
 void eepromWriteInt(int adr, int wert) {
-  byte low = wert & 0xFF;
-  byte high = (wert >> 8) & 0xFF;
-  EEPROM.write(adr, low); // dauert 3,3ms
-  EEPROM.write(adr + 1, high);
-  return;
+	byte low = wert & 0xFF;
+	byte high = (wert >> 8) & 0xFF;
+	EEPROM.write(adr, low); // dauert 3,3ms
+	EEPROM.write(adr + 1, high);
+	return;
 }
 
 int eepromReadInt(int adr) {
-  byte low = EEPROM.read(adr);
-  byte high = EEPROM.read(adr + 1);
-  return low + ((high << 8) & 0xFF00);
+	byte low = EEPROM.read(adr);
+	byte high = EEPROM.read(adr + 1);
+	return low + ((high << 8) & 0xFF00);
 }
 
 void calibrate() {
-  boolean calibrating = true;
-  while (calibrating) {
-    int prior = analogRead(A1);
-    extend(26);
-    delay(200);
-    int after = analogRead(A1);
-    if (abs(prior - after) < deadBand * 2) {
-      min = (prior + after) / 2;
-      calibrating = false;
-    }
-  }
-  calibrating = true;
-  while (calibrating) {
-    int prior = analogRead(A1);
-    retract(26);
-    delay(200);
-    int after = analogRead(A1);
-    if (abs(prior - after) < deadBand * 2) {
-      max = (prior + after) / 2;
-      calibrating = false;
-    }
-  }
-  Serial.print("min ");
-  Serial.print(min);
-  Serial.print(" max ");
-  Serial.println(max);
-  eepromWriteInt(EEPROM_MIN_MAX_ADDR_OFFSET + 0, min);
-  eepromWriteInt(EEPROM_MIN_MAX_ADDR_OFFSET + 2, max);
-  targetPos = (max - min) / 2 + min;
+	boolean calibrating = true;
+	while (calibrating) {
+		int prior = analogRead(A1);
+		extend(26);
+		delay(200);
+		int after = analogRead(A1);
+		if (abs(prior - after) < deadBand * 2) {
+			min = (prior + after) / 2;
+			calibrating = false;
+		}
+	}
+	calibrating = true;
+	while (calibrating) {
+		int prior = analogRead(A1);
+		retract(26);
+		delay(200);
+		int after = analogRead(A1);
+		if (abs(prior - after) < deadBand * 2) {
+			max = (prior + after) / 2;
+			calibrating = false;
+		}
+	}
+	Serial.print("min ");
+	Serial.print(min);
+	Serial.print(" max ");
+	Serial.println(max);
+	eepromWriteInt(EEPROM_MIN_MAX_ADDR_OFFSET + 0, min);
+	eepromWriteInt(EEPROM_MIN_MAX_ADDR_OFFSET + 2, max);
+	targetPos = (max - min) / 2 + min;
 }
 
 int deltaDriveCalc(int delta) {
-  if (delta > DELTA_DRIVE_START) {
-    return 0;
-  }
-  int pwr = map(delta, 0, DELTA_DRIVE_START, DELTA_DRIVE_MIN_PWR, 0);
-  Serial.print(delta);
-  Serial.print(" ");
-  Serial.println(pwr);
-  return pwr;
+	if (delta > DELTA_DRIVE_START) {
+		return 0;
+	}
+	int pwr = map(delta, 0, DELTA_DRIVE_START, DELTA_DRIVE_MIN_PWR, 0);
+	return pwr;
 }
 
 void retract(int delta) {
-  analogWrite(EXTEND, 255);
-  analogWrite(RETRACT, deltaDriveCalc(delta));
+	analogWrite(EXTEND, 255);
+	analogWrite(RETRACT, deltaDriveCalc(delta));
 }
 
 void extend(int delta) {
-  analogWrite(EXTEND, deltaDriveCalc(delta));
-  analogWrite(RETRACT, 255);
+	analogWrite(EXTEND, deltaDriveCalc(delta));
+	analogWrite(RETRACT, 255);
 }
 
 void stop() {
-  analogWrite(EXTEND, 255);
-  analogWrite(RETRACT, 255);
+	analogWrite(EXTEND, 255);
+	analogWrite(RETRACT, 255);
 }
 
 void setup() {
-  for (int thisReading = 0; thisReading < NUM_READINGS; thisReading++) {
-    readings[thisReading] = 0;
-  }
 
-  FastLED.addLeds<WS2812, LED_DATA, GRB>(leds, 1);
-  leds[0] = CRGB::Green;
-  FastLED.setBrightness(64);
-  FastLED.show();
+	pinMode(13, INPUT_PULLUP);
 
+	FastLED.addLeds<WS2812, LED_DATA, GRB>(leds, 1);
+	leds[0] = CRGB(0,32,0);
+	FastLED.setBrightness(64);
+	FastLED.show();
 
-  Serial.begin(115200);
-  pinMode(A1, INPUT);
+	Serial.begin(115200);
+	pinMode(A1, INPUT);
 
-  min = eepromReadInt(0);
-  max = eepromReadInt(2);
-  Serial.print("reading calibration min ");
-  Serial.print(min);
-  Serial.print(" max ");
-  Serial.println(max);
-  if (min + 200 > max) {
-    calibrate();
-  }
+	min = eepromReadInt(0);
+	max = eepromReadInt(2);
+	Serial.print("reading calibration min ");
+	Serial.print(min);
+	Serial.print(" max ");
+	Serial.println(max);
+	if (min + 200 > max) {
+		calibrate();
+	}
 
-  PCICR |= (1 << PCIE0);          //Set PCIE0 to enable PCMSK0 scan.
-  PCMSK0 |= B00000101;
+	PCICR |= (1 << PCIE0);          //Set PCIE0 to enable PCMSK0 scan.
+	PCMSK0 |= B00000101;
+
+	curPos = analogRead(A1);
 }
 
 void loop() {
-  total = total - readings[readIndex];
-  readings[readIndex] = analogRead(A1);
-  total = total + readings[readIndex];
-  readIndex = readIndex + 1;
-  if (readIndex >= NUM_READINGS) {
-    readIndex = 0;
-  }
-  int curPos = total / NUM_READINGS;
 
-  limit_receiver_input(CHANNEL_1);
-  limit_receiver_input(CHANNEL_2);
-  targetPos = map(receiver_input[0], 1000, 2000, min, max);
+	if (!digitalRead(13)) {
+		calibrateCount++;
+		leds[0] = CRGB::Aqua;
+		Serial.println(calibrateCount);
+		if (calibrateCount== 2000) {
+			calibrateCount = 0;
+			leds[0] = CRGB::Yellow;
+			calibrate();
+		}
+	} else {
+		calibrateCount = 0;
+	}
 
-  int delta = abs(targetPos - curPos);
+	curPos = curPos * 0.95 + analogRead(A1) * 0.05;
+	if (raw_inputs[0] > 900 && raw_inputs[0] < 2100)
+		targetPos = targetPos * 0.8
+				+ map(raw_inputs[0], 1000, 2000, min, max) * 0.2;
 
-  if (blocking) {
-    if(--blockingCount == 0) blocking = false;
-    stop();
-  } else {
-    
+	//Serial.println(raw_inputs[0]);
 
+	int delta = abs(targetPos - curPos);
 
-    if (curPos < (targetPos - deadBand)) {
-      retract(delta);
-      leds[0] = CRGB::Red;
-    } else if (curPos > (targetPos + deadBand)) {
-      extend(delta);
-      leds[0] = CRGB::Blue;
-    } else {
-      stop();
-      leds[0] = CRGB::Green;
-    }
-    FastLED.show();
-  }
-  lastPos = curPos;
+	if (blocking) {
+		if (--blockingCount == 0)
+			blocking = false;
+		stop();
+	} else {
+		if (curPos < (targetPos - deadBand)) {
+			retract(delta);
+			if (calibrateCount < 5)
+				leds[0] = CRGB::Red;
+		} else if (curPos > (targetPos + deadBand)) {
+			extend(delta);
+			if (calibrateCount < 5)
+				leds[0] = CRGB::Blue;
+		} else {
+			stop();
+			leds[0] = CRGB(0, 32, 0);
+		}
+	}
+	lastPos = curPos;
 }
 
 ISR(PCINT0_vect) {
-  current_time_int0 = micros();
+	current_time_int0 = micros();
 
-  //Channel 1
-  if (PINB & B00000001) {
-    if (last_flank[CHANNEL_1] == 0) {
-      last_flank[CHANNEL_1] = 1;
-      upflank_time[CHANNEL_1] = current_time_int0;
-    }
-  }
-  else if (last_flank[CHANNEL_1] == 1) {
-    last_flank[CHANNEL_1] = 0;
-    raw_inputs[CHANNEL_1] = current_time_int0 - upflank_time[CHANNEL_1];
-  }
+	//Channel 1
+	if (PINB & B00000001) {
+		if (last_flank[CHANNEL_1] == 0) {
+			last_flank[CHANNEL_1] = 1;
+			upflank_time[CHANNEL_1] = current_time_int0;
+		}
+	} else if (last_flank[CHANNEL_1] == 1) {
+		last_flank[CHANNEL_1] = 0;
+		raw_inputs[CHANNEL_1] = current_time_int0 - upflank_time[CHANNEL_1];
+		FastLED.show();
+	}
 
-  //Channel 2
-  if (PINB & B00000100) {
-    if (last_flank[CHANNEL_2] == 0) {
-      last_flank[CHANNEL_2] = 1;
-      upflank_time[CHANNEL_2] = current_time_int0;
-    }
-  }
-  else if (last_flank[CHANNEL_2] == 1) {
-    last_flank[CHANNEL_2] = 0;
-    raw_inputs[CHANNEL_2] = current_time_int0 - upflank_time[CHANNEL_2];
-  }
+	//Channel 2
+	if (PINB & B00000100) {
+		if (last_flank[CHANNEL_2] == 0) {
+			last_flank[CHANNEL_2] = 1;
+			upflank_time[CHANNEL_2] = current_time_int0;
+		}
+	} else if (last_flank[CHANNEL_2] == 1) {
+		last_flank[CHANNEL_2] = 0;
+		raw_inputs[CHANNEL_2] = current_time_int0 - upflank_time[CHANNEL_2];
+	}
 }

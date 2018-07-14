0ad45328086dc0cd5189fd1f3d29ad757a48d7cf
Marcel Rupp <marcel.rupp@live.de>
2018-07-14 02:24:04 +0200

    SoftWire works ™

--- MainController/MCP4725.cpp
+++ MainController/MCP4725.cpp
@@ -1,6 +1,6 @@
 #include "Arduino.h"
-#include <Wire.h>
 #include "MCP4725.h"
+
 /*See page 19 of the MCP4726 DataSheet.
  *[C2,C1]=00 -- Fast Mode
  *[C2,C1,C0]=010 --- Write to DAC register only
@@ -28,10 +28,14 @@
  *in the file twi.c. The register TWAR is set to (address << 1). The R/W bit (bit 8) is actually determined based 
  *on the function you send after .begin()
  */
+
 MCP4725::MCP4725() {}
-void MCP4725::begin(uint8_t addr) { //in the Arduino code, we'd send 0x62 as arumenment when A0_bit=GND (default).
-  _i2caddr = addr;                    //or if A0_bit=vcc then we would pass 0x63 as argument
-  Wire.begin();
+
+void MCP4725::begin(uint8_t addr, SoftWire* _sw) {
+  _i2caddr = addr;
+  sw = _sw;
+  sw->setRxBuffer(&buffer, 64);
+  sw->setTxBuffer(&buffer, 64);
 }
 void MCP4725::setFastMode(){
 #ifdef TWBR // in case this library is used on a chip that does not have TWBR reg defined.  
@@ -45,100 +49,38 @@ void MCP4725::setFastMode(){
 void MCP4725::setVoltageAndSave(uint16_t output){
  /*For Normal-Speed: (Only normal speed includes the C3 bit, needed for writing to EEPROM)
   *[Addr.Byte]+[C2,C1,C0,x,x,PD1,PD0,x]+[D11,D10,D9,D8,D7,D6,D5,D4]+[D3,D2,D1,D0,x,x,x,x]  */
-  Wire.beginTransmission(_i2caddr);
-  Wire.write(WRITEDACEEPROM); //[C2,C1,C0,x,x,PD1,PD0,x]=[0,1,1,0,0,0,0,0]
+  sw->beginTransmission(_i2caddr);
+  sw->write(WRITEDACEEPROM); //[C2,C1,C0,x,x,PD1,PD0,x]=[0,1,1,0,0,0,0,0]
   /*The 12-bit output is in 16-bit form (0,0,0,0,D11.D10.D9.D8.D7.D6.D5.D4,D3.D2.D1.D0).*/
   uint8_t firstbyte=(output>>4);//(0,0,0,0,0,0,0,0,D11.D10.D9.D8.D7.D6.D5.D4) of which only the 8 LSB's survive
   output = output << 12;	//(D3.D2.D1.D0,0,0,0,0,0,0,0,0,0,0,0,0) 
   uint8_t secndbyte=(output>>8);//(0,0,0,0,0,0,0,0,D3,D2,D1,D0,0,0,0,0)	of which only the 8 LSB's survive.
-  Wire.write(firstbyte); 
-  Wire.write(secndbyte);
-  Wire.endTransmission();
+  sw->write(firstbyte);
+  sw->write(secndbyte);
+  sw->endTransmission();
 }
 void MCP4725::setVoltage(uint16_t output){
  /*For Normal-Speed:
   *[Addr.Byte]+[C2,C1,C0,x,x,PD1,PD0,x]+[D11,D10,D9,D8,D7,D6,D5,D4]+[D3,D2,D1,D0,x,x,x,x]  */
-  Wire.beginTransmission(_i2caddr);
-  Wire.write(WRITEDAC); //[C2,C1,C0,x,x,PD1,PD0,x]=[0,1,0,0,0,0,0,0]
+	sw->beginTransmission(_i2caddr);
+  sw->write(WRITEDAC); //[C2,C1,C0,x,x,PD1,PD0,x]=[0,1,0,0,0,0,0,0]
   /*The 12-bit output is in 16-bit form (0,0,0,0,D11.D10.D9.D8.D7.D6.D5.D4,D3.D2.D1.D0).*/
   uint8_t firstbyte=(output>>4);//(0,0,0,0,0,0,0,0,D11.D10.D9.D8.D7.D6.D5.D4) of which only the 8 LSB's survive
   output = output << 12;	//(D3.D2.D1.D0,0,0,0,0,0,0,0,0,0,0,0,0) 
   uint8_t secndbyte=(output>>8);//(0,0,0,0,0,0,0,0,D3,D2,D1,D0,0,0,0,0)	of which only the 8 LSB's survive.
-  Wire.write(firstbyte); 
-  Wire.write(secndbyte);
-  Wire.endTransmission();
+  sw->write(firstbyte);
+  sw->write(secndbyte);
+  sw->endTransmission();
 }
 void MCP4725::setVoltageFast( uint16_t output){
  /*For Fast-Speed:
   *[Addr.Byte]+[C2,C1,PD1,PD0,D11,D10,D9,D8],[D7,D6,D5,D4,D3,D2,D1,D0]  */
-  Wire.beginTransmission(_i2caddr);
+	sw->beginTransmission(_i2caddr);
   //output is a 12-bit value in 16-bit form, namely: [0,0,0,0,D11,D10,D9,D8,D7,D6,D5,D4,D3,D2,D1,D0]
   uint8_t firstbyte=(output>>8); //[0,0,0,0,0,0,0,0,0,0,0,0,D11,D10,D9,D8] only the 8 LSB's survive
   uint8_t secndbyte=(output); //only the 8 LSB's survive.
-  Wire.write(firstbyte);  // Upper data bits (0,0,0,0,D11,D10,D9,D8)       
-  Wire.write(secndbyte);  // Lower data bits (D7,D6,D5,D4,D3,D2,D1,D0)
-  Wire.endTransmission();
-}
-void MCP4725::powerDown1kPullDown(){ //[PD1,PD0]=01; [C2,C1,C0]=010 - Write to DAC only
- /*For Normal-Speed:
-  *[Addr.Byte]+[C2,C1,C0,x,x,PD1,PD0,x]+[D11,D10,D9,D8,D7,D6,D5,D4]+[D3,D2,D1,D0,x,x,x,x]  */
-  Wire.beginTransmission(_i2caddr);
-  Wire.write(0b01000010);
-  Wire.write(0b00000000);
-  Wire.write(0b00000000);
-  Wire.endTransmission();
-}
-void MCP4725::powerDown100kPullDown(){//[PD1,PD0]=10; [C2,C1,C0]=010 - Write to DAC only
- /*For Normal-Speed:
-  *[Addr.Byte]+[C2,C1,C0,x,x,PD1,PD0,x]+[D11,D10,D9,D8,D7,D6,D5,D4]+[D3,D2,D1,D0,x,x,x,x]  */
-  Wire.beginTransmission(_i2caddr);
-  Wire.write(0b01000100);
-  Wire.write(0b00000000);
-  Wire.write(0b00000000);
-  Wire.endTransmission();  
-}
-void MCP4725::powerDown500kPullDown(){//[PD1,PD0]=11; [C2,C1,C0]=010 - Write to DAC only
- /*For Normal-Speed:
-  *[Addr.Byte]+[C2,C1,C0,x,x,PD1,PD0,x]+[D11,D10,D9,D8,D7,D6,D5,D4]+[D3,D2,D1,D0,x,x,x,x]  */
-  Wire.beginTransmission(_i2caddr);
-  Wire.write(0b01000110);
-  Wire.write(0b00000000);
-  Wire.write(0b00000000);
-  Wire.endTransmission();
+  sw->write(firstbyte);  // Upper data bits (0,0,0,0,D11,D10,D9,D8)
+  sw->write(secndbyte);  // Lower data bits (D7,D6,D5,D4,D3,D2,D1,D0)
+  sw->endTransmission();
 }
 
-uint16_t MCP4725::readCurrentDacVal(){
-  Wire.requestFrom(_i2caddr, (uint8_t) 5);
-  Wire.read(); //status
-  uint8_t upper8bits = Wire.read(); //DAC Register data (D11,D10,D9,D8,D7,D6,D5,D4)
-  uint8_t lower8bits = Wire.read(); //DAC Register data (D3,D2,D1,D0,0,0,0,0)
-  Wire.read(); //forth returned byte is EEPROM data (x,PD1,PD0,x,D11,D10,D9,D8) 
-  Wire.read(); //fifth returned byte is EEPROM data (D7,D6,D5,D4,D3,D2,D1,D0)
-
-  /*We now need to return the value (0,0,0,0,D11,D10,D9,D8,D7,D6,D5,D4,D3,D2,D1,D0). */
-  return (upper8bits<<4) | (lower8bits>>4); //This is how we get the 16-bit result we want to return.
-}
-
-
-uint16_t MCP4725::readValFromEEPROM(){
-  Wire.requestFrom(_i2caddr, (uint8_t) 5);
-  while(Wire.available()!=5){
-    Serial.println("Waiting for readValFromEEPROM() to complete.");
-    //just wait for a while until the DAC sends the data to the reabuffer
-  }
-  uint8_t statusBit = Wire.read() >> 7; 
-  Wire.read(); //secnd returned byte is DAC Register data (upper 8 bits)
-  Wire.read(); //third returned byte is DAC Register data (lower 4 bits + 0000)
-  uint8_t upper8bits = Wire.read(); //forth returned byte is EEPROM data (x,PD1,PD0,x,D11,D10,D9,D8)
-  uint8_t lower8bits = Wire.read(); //fifth returned byte is EEPROM data (D7,D6,D5,D4,D3,D2,D1,D0)
-  
-  if(statusBit==0){
-    Serial.println("Currently writing to EEPROM. Trying to read again...");
-    return readValFromEEPROM();
-  }
-  else{
-    /*We now need to return the value (0,0,0,0,D11,D10,D9,D8,D7,D6,D5,D4,D3,D2,D1,D0). */
-    upper8bits = upper8bits & 0b00001111; //clear the first 4 bits.
-    return (upper8bits<<8) | lower8bits; //This is how we get the 16-bit result we want to return.
-  }
-}
--- MainController/MCP4725.h
+++ MainController/MCP4725.h
@@ -1,5 +1,5 @@
 #include "Arduino.h"
-#include <Wire.h>
+#include <SoftWire.h>
 
 #define WRITEDAC        (0x40)  //(0100 0000) Writes to the DAC
 #define WRITEDACEEPROM  (0x60)  //(0110 0000) Writes to the DAC and the EEPROM (persisting the assigned value after reset)
@@ -7,16 +7,13 @@
 class MCP4725{
  public:
   MCP4725();
-  void begin(uint8_t a);  
+  void begin(uint8_t addr, SoftWire* sw);
   void setFastMode();  
   void setVoltageAndSave(uint16_t output);
   void setVoltage(uint16_t output);
   void setVoltageFast(uint16_t output);
-  void powerDown500kPullDown();
-  void powerDown100kPullDown();
-  void powerDown1kPullDown();
-  uint16_t readValFromEEPROM();
-  uint16_t readCurrentDacVal();
  private:
+  uint8_t buffer[64];
+  SoftWire* sw;
   uint8_t _i2caddr;
 };

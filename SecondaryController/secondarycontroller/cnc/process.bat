f5cec332aaa1b613d5c52ba3549a99ce8c13bb1f
empirephoenix <empirephoenix@yahoo.de>
2018-11-06 22:42:40 +0100

    secondary controller mk2

---
+++ SecondaryController/secondarycontroller/cnc/process.bat
@@ -0,0 +1,2 @@
+C:\Users\empire\Desktop\pcb2gcode-1.3.2-win64\pcb2gcode.exe --extra-passes 1 --onedrill --zero-start --metricoutput --nog81 --nog64 --metric --cut-infeed 1 --cut-speed 1 --cut-feed 1 --zcut -3 --outline secondarycontroller-Edge.Cuts.gbr --back secondarycontroller-B.Cu.gbr --front secondarycontroller-F.Cu.gbr --cut-side back --drill-side back --drill secondarycontroller-PTH.drl --drill-speed 1000 --drill-feed 3 --zdrill 0 --zsafe 8 --cutter-diameter 0.5 --zchange 20 --zwork -0.2 --offset 0.1 --mill-feed 30 --mill-speed 1000
+pause
